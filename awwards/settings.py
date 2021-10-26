47c7d94cde6fb4bc6f1519a33faeed2b0e2f1009
Ephraim Bundi <ebundi04@gmail.com>
2021-10-26 14:55:40 +0300

    Add crispy forms

--- awwards/settings.py
+++ awwards/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'crispy_forms',
     'ratings',
     'django_registration',
     'bootstrap5',
